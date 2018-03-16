1d8aa6acf1632e7bfd24fa8bb47b91e61cf791bb
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-03-16 18:19:01 -0400

    make home component with mission statement and pending carousel.

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                Hello World! Carousel Pending...
+                <div>
+                    <h1 id="missionStatement">Mission Statement</h1>
+                    <p className="missionParagraphs">Our mission at Tutelage is to serve as a tool to spark a connection, preferably a meaningful, mutually beneficial, long-term one between a mentor with significant experience and a mentee. Whether you are seeking to gain new skills from someone who has already travelled on the road of the journey you are just beginning, or if you are a seasoned professional seeking to give back by donating time, resources, advice and expertise to a younger version of yourself, Tutelage fosters an environment for professional life enrichment. </p>
+                    <p className="missionParagraphs">We enjoy when people enjoy the instruction,teaching and training they receive in conjunction with personalized coaching, guidance, nurturance, and encouragement from those they receive it from.</p>
+                    <p className="missionParagraphs">We specialize in creating "win-win" scenarios for both sides. The mentee recieves the crucial assistance needed to "level-up" their skills to improve their income, gain promotions that better their career trajectory, and helps them to navigate around the "potholes" of mistakes that others have encountered. The mentor, on the other hand, much more than intrinsic value, not only experiences the joys of altruism by selflessly giving to others in need, but also reinforces their own learning by way of teaching others, and may also even find themselves grooming the dream candidate that they were seeking to hire at their own business. </p>
+                    <p className="missionParagraphs">A Mentor is more than just a tutor. A mentor is a conduit of life transformation. The countless number of lives that have been enriched as a result of the meeting of the minds through our site and their impact on various industries is immeasurable. </p>
+                    <p className="missionParagraphs">Reach Out To Your Full Potential.</p>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Home;
