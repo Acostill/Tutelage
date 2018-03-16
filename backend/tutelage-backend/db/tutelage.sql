d0d5699157e583c5fe3639c3c17e8d78621bce58
Gregory Davis <davisgreg1@gmail.com>
2018-03-16 17:37:53 -0400

    added backend

---
+++ backend/tutelage-backend/db/tutelage.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS tutelage;
+CREATE DATABASE tutelage;
+
+\c tutelage;
+
+CREATE TABLE users (
+  ID SERIAL PRIMARY KEY,
+  username VARCHAR (50) UNIQUE NOT NULL,
+  firstname VARCHAR NOT NULL,
+  lastname VARCHAR NOT NULL,
+  email VARCHAR,
+  password_digest VARCHAR,
+  ismentor boolean
+);
+
+/*CREATE TABLE messages (
+  ID SERIAL PRIMARY KEY,
+  sender VARCHAR,
+  recipient VARCHAR,
+  body VARCHAR
+);
+*/
+
+/* tyler, password: 123456 */
+
+INSERT INTO users (username, firstname, lastname, email, password_digest, ismentor)
+  VALUES ('TestMentee1','Greg','Davis','davisgreg1@gmail.com', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','true')
