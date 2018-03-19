3d78e10168876265df2228e771ab7ce069cf76e7
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-03-19 14:52:19 -0400

    create register user form and link route to register page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 
 import { Route, Link, Switch } from "react-router-dom";
 import LoginUser from './components/users/login/LoginUser';
+import RegisterUser from './components/users/login/RegisterUser';
 
 class App extends Component {
   render() {
@@ -12,11 +13,15 @@ class App extends Component {
       <div>
       <nav>
         <Link to="/"> Tutelage </Link>
-        <Link to="/login">Register / Log In</Link>
+        {" "}
+        <Link to="/login"> Log In </Link>
+        {" "}
+        <Link to="/register"> Register </Link>
       </nav>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={LoginUser} />
+        <Route path="/register" component={RegisterUser} />
       </Switch>
       </div>
     );
---
+++ frontend/src/components/users/login/RegisterUser.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+
+class RegisterUser extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div id="registerForm">
+            <fieldset>
+                <legend>Register New User:</legend>
+                <form>
+                    <form>
+                        Are you a: {" "} {" "}
+                        <input type="radio" name="usertype" value="mentor" checked/>Mentor {" "} {" "}
+                        <input type="radio" name="usertype" value="mentee" checked/>Mentee<br />
+                    </form>
+
+                    <br />
+                        <input type="text" placeholder="First Name" name="firstname" /> <br />
+                    <br />
+                        <input type="text" placeholder="Last Name" name="lastname" /> <br />
+                    <br />
+                        <input type="email" placeholder="Email" name="email" /> <br />
+                    <br />
+                        <input type="password" placeholder="Password" name="password" />
+                    <br />
+                    <br />
+
+                        <input type="submit" value="Create Account" />
+                </form>
+            </fieldset>
+
+            <div>
+                {/* Will come back for this */}
+                {" "}
+                <br />
+                <p>Already a Member? Login <a href="/login">HERE</a></p>
+
+            </div>
+                
+            </div>
+        )
+    }
+
+
+}
+
+
+export default RegisterUser;
