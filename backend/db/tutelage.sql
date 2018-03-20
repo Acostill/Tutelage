8f3bb6fd703114148f034da8f7a699ef52bf074c
Gregory Davis <davisgreg1@gmail.com>
2018-03-20 19:57:54 -0400

    designed the api for questions and possible answers

--- backend/db/tutelage.sql
+++ backend/db/tutelage.sql
@@ -8,6 +8,7 @@ CREATE TABLE users (
   username VARCHAR (50) UNIQUE NOT NULL,
   firstname VARCHAR NOT NULL,
   lastname VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
   password_digest VARCHAR,
   ismentor boolean
 );
@@ -15,10 +16,10 @@ CREATE TABLE users (
 CREATE TABLE questions (
 	ID SERIAL PRIMARY KEY,
 	the_question VARCHAR,
-	answer_1 INTEGER,
-	answer_2 INTEGER,
-	answer_3 INTEGER,
-	answer_4 INTEGER
+	answer_1 VARCHAR,
+	answer_2 VARCHAR,
+	answer_3 VARCHAR,
+	answer_4 VARCHAR
 );
 
 CREATE TABLE answers (
@@ -31,30 +32,23 @@ CREATE TABLE answers (
 
 /* tyler, password: 123456 */
 
-INSERT INTO users (username, firstname, lastname, password_digest, ismentor)
-  values ('test1','Greg1','Davis1', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','true');
-INSERT INTO users (username, firstname, lastname, password_digest, ismentor)
-  values ('test2','Greg2','Davis2', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','true');
-INSERT INTO users (username, firstname, lastname, password_digest, ismentor)
-  values ('test3','Greg3','Davis3', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','false');
-INSERT INTO users (username, firstname, lastname, password_digest, ismentor)
-  values ('test4','Greg4','Davis4', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','false');
+INSERT INTO users (username, firstname, lastname, email, password_digest, ismentor)
+  values ('test1','Greg1','Davis1', 'me@u.we', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','true');
+INSERT INTO users (username, firstname, lastname, email, password_digest, ismentor)
+  values ('test2','Greg2','Davis2', 'me@u.we', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','true');
+INSERT INTO users (username, firstname, lastname, email, password_digest, ismentor)
+  values ('test3','Greg3','Davis3', 'me@u.we','$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','false');
+INSERT INTO users (username, firstname, lastname, email, password_digest, ismentor)
+  values ('test4','Greg4','Davis4', 'me@u.we','$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq','false');
 
 INSERT INTO questions (the_question, answer_1, answer_2, answer_3, answer_4)
-  values ('Do you like Math?',1, 2, 3, 4);
+  values ('Do you like Math?','a little bit', 'Im learning to like it', 'yes I like it', 'I love love Math');
 INSERT INTO questions (the_question, answer_1, answer_2, answer_3, answer_4)
-  values ('Do you like Reading?',1, 2, 3, 4);
+  values ('Do you like Reading?','a little bit', 'Im learning to like it', 'yes I like it', 'I love love Reading');
 INSERT INTO questions (the_question, answer_1, answer_2, answer_3, answer_4)
-  values ('Do you like to exercise?',1, 2, 3, 4);
+  values ('Do you like to exercise?','a little bit', 'Im learning to like it', 'yes I like it', 'I love love Exercising');
 INSERT INTO questions (the_question, answer_1, answer_2, answer_3, answer_4)
-  values ('Are you a psychopathic murderer lacking remorse and empathy?',1, 2, 3, 4);
-
+  values ('Are you a psychopathic murderer lacking remorse and empathy?','a little bit', 'Im learning to like it', 'yes I like it', 'I love love Evility');
 
 INSERT INTO answers (answer_selection, question_id, user_id)
   values (1, 1, 1);
-INSERT INTO answers (answer_selection, question_id, user_id)
-  values (2, 2, 2);
-INSERT INTO answers (answer_selection, question_id, user_id)
-  values (3, 3, 3);
-INSERT INTO answers (answer_selection, question_id, user_id)
-  values (4, 4, 4);
