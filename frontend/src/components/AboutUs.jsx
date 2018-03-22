82755f5321d8cbe1e18efb7a708ef7b04297cde8
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-03-21 20:38:50 -0400

    create about us page with route and link.

---
+++ frontend/src/components/AboutUs.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class AboutUs extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                Here's the About Us Page!
+            </div>
+        )
+    }
+
+}
+
+
+
+
+export default AboutUs;
