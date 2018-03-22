76ab78ae06a4a00d513db2b2e5c3a82a658288b7
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-03-22 16:02:21 -0400

    make improvements to about us page.

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import '../css/AboutUs.css';
 
 class AboutUs extends Component {
     constructor() {
@@ -9,11 +9,77 @@ class AboutUs extends Component {
     render() {
         return (
             <div id="mainDiv">
-                <div class="teamPhotos">
-                    <img width={250} height={250} src="./images/GersonCropped.jpg" alt="Tech Lead"/>
-                </div>
+                    <div class="teamMember">
+                        <div class="teamMemberTitle">
+                            <h3>Technical Lead</h3>
+                        </div>
+                        <div class="teamMemberPhoto">
+                            <img width={250} height={250} src="./images/GersonCropped.jpg" alt="Tech Lead"/>
+                        </div>
+                        <div class="teamMemberCaption">
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</h5>
+                        </div>
+                    </div>
 
-                <div class="teamPhotos">
+
+                    <div class="teamMember">
+                        <div class="teamMemberTitle">
+                            <h3>Product Manager</h3>
+                        </div>
+                        <div class="teamMemberPhoto">
+                            <img width={250} height={250} src="./images/gregcropped.jpg" alt="Product Manager"/>
+                        </div>
+                        <div class="teamMemberCaption">
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</h5>
+                        </div>
+                    </div>
+
+
+                    <div class="teamMember">
+                        <div class="teamMemberTitle">
+                            <h3>Design Lead</h3>
+                        </div>
+                        <div class="teamMemberPhoto">
+                            <img width={250} height={250} src="./images/CarolinaPic.jpeg" alt="Design Lead"/>
+                        </div>
+                        <div class="teamMemberCaption">
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</h5>
+                        </div>
+                    </div>
+
+
+                    <div class="teamMember">
+                        <div class="teamMemberTitle">
+                            <h3>Demo Lead</h3>
+                        </div>
+                        <div class="teamMemberPhoto">
+                            <img width={250} height={250} src="./images/EddieCropped.jpg" alt="Demo Lead"/>
+                        </div>
+                        <div class="teamMemberCaption">
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</h5>
+                        </div>
+                    </div>
+
+
+                     <div class="teamMember">
+                        <div class="teamMemberTitle">
+                            <h3>Scrum Master</h3>
+                        </div>
+                        <div class="teamMemberPhoto">
+                            <img width={250} height={250} src="./images/NickCropped.jpg" alt="Scrum Master"/>
+                        </div>
+                        <div class="teamMemberCaption">
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</h5>
+                        </div>
+                    </div>
+
+
+                    
+
+
+
+
+                {/* <div class="teamPhotos">
                     <img width={250} height={250} src="./images/gregcropped.jpg" alt="Product Manager"/>
                 </div>
 
@@ -27,7 +93,7 @@ class AboutUs extends Component {
 
                 <div class="teamPhotos">
                 <img width={250} height={250} src="./images/NickCropped.jpg" alt="Scrum Master"/>
-                </div>
+                </div> */}
 
             </div>
         )
