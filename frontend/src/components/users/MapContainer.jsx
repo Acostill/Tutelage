dcad08a28fd2d7c9566486080d8bb1cd9613cf58
Gregory Davis <davisgreg1@gmail.com>
2018-03-22 23:26:06 -0400

    added google map api

--- frontend/src/components/users/MapContainer.jsx
+++ frontend/src/components/users/MapContainer.jsx
@@ -1,24 +1,64 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import React from 'react';
- 
-export class MapContainer extends React.Component {
-render() {
+import React, { Component } from "react";
+import {
+  Map,
+  InfoWindow,
+  Marker,
+  GoogleApiWrapper,
+  google
+} from "google-maps-react";
+import { Redirect } from "react-router";
+export class MapContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: false,
+      lat_longs: [],
+      username: ""
+    };
+  }
+
+  onMapClicked = username => {
+    this.setState({
+      selected: true,
+      username: username
+    });
+  };
+
+  render() {
+    const { selected, username, lat_longs } = this.state;
+
+    if (selected) {
+      return <Redirect to={`/users/${username}`} />;
+      this.setState({
+        selected: false
+      });
+    }
+
+    const { arrOfLatLongs } = this.props;
     return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
+      <div id="map"width="150px"height="150px">
+        <Map
+          google={this.props.google}
+          initialCenter={{
+            lat: 40.7128,
+            lng: -73.935242
+          }}
+          zoom={9}
+        >
+          {arrOfLatLongs.map(latLong => (
+            <Marker
+              title={"user"}
+              name={"user"}
+              position={{ lat: latLong.latitude, lng: latLong.longitude }}
+              onClick={() => this.onMapClicked()}
+            />
+          ))}
+        </Map>
+      </div>
     );
   }
 }
- 
+
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyAMySR4qXxtMWeAhlQVz5IzFwOPoG7dBO0')
-})(MapContainer)
+  apiKey: "AIzaSyAMySR4qXxtMWeAhlQVz5IzFwOPoG7dBO0"
+})(MapContainer);
--- frontend/src/components/users/SearchUsers.jsx
+++ frontend/src/components/users/SearchUsers.jsx
@@ -1,45 +1,63 @@
 import React, { Component } from "react";
-import { Redirect } from 'react-router';
+import { Redirect } from "react-router";
 import { Route, Link, Switch } from "react-router-dom";
 import axios from "axios";
+import Map from "./MapContainer";
 import "../../css/SearchUsers.css";
+import zipcodes from "zipcodes";
 
 class SearchUsers extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      zip_codes: [],
+      lat_longs: []
     };
   }
 
   handleCardClick = e => {
     let username = e.target.value;
     console.log(e.target.value);
-  }
+  };
 
   getUserList = () => {
     axios
-    .get("/users/search")
-    .then(res => {
-      console.log("res.data", res.data.data);
-      this.setState({
-        users: res.data.data
+      .get("/users/search")
+      .then(res => {
+        console.log("res.data", res.data.data);
+        let zipz = res.data.data.map(elem => {
+          return elem.zipcode;
+        });
+        this.setState({
+          users: res.data.data,
+          zip_codes: zipz
+        });
+      })
+      .catch(err => {
+        console.log("err", err);
       });
+  };
+
+  getLatLongs = () => {
+    const { zip_codes } = this.state;
+    let newArr = zipcodes.map((elem,idx)=>{
+      let userLatLongs = zipcodes.lookup(elem)
+      return userLatLongs.latitude
     })
-    .catch(err => {
-      console.log("err", err);
-    });
+    this.setState({
+      lat_longs: newArr
+    })
   }
 
   componentDidMount() {
-    this.getUserList()
+    this.getUserList();
   }
 
-
-
   render() {
+    console.log("state:", this.state);
     const { handleCardClick } = this;
-    const { users } = this.state;
+    const { users, lat_longs, zip_codes } = this.state;
     const style = {
       width: "128px",
       height: "128px"
@@ -52,9 +70,15 @@ class SearchUsers extends Component {
         {users.map(user => {
           const { imgurl, firstname, lastname, username, location } = user;
           return (
-            <div className='profile-card' >
-              <Link to={`/users/${username}`}> <img src={imgurl} alt="user_pic" style={style} /> </Link>
-              <Link to={`/users/${username}`}> <h2>{username}</h2> </Link>
+            <div className="profile-card">
+              <Link to={`/users/${username}`}>
+                {" "}
+                <img src={imgurl} alt="user_pic" style={style} />{" "}
+              </Link>
+              <Link to={`/users/${username}`}>
+                {" "}
+                <h2>{username}</h2>{" "}
+              </Link>
               <h3>
                 {firstname} {lastname}
               </h3>
@@ -62,6 +86,9 @@ class SearchUsers extends Component {
             </div>
           );
         })}
+        <div className="gmap">
+          <Map arrOfLatLongs={lat_longs} />
+        </div>
       </div>
     );
   }
