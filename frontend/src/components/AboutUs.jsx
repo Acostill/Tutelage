e4d59a0890accc8c17e8efb5d578c87683e0a486
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2018-03-27 13:02:05 -0400

    preliminary updates to carousel captions, button enticement, and switch to about us mission statement placement.

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -9,67 +9,89 @@ class AboutUs extends Component {
     render() {
         return (
             <div>
+
+                <div id="missionStatementSection">
+                    <h1 id="missionStatement">Mission Statement</h1>
+                    <h2 className="missionParagraphs">Our mission at Tutelage is to serve as a tool to spark a connection, preferably a meaningful, mutually beneficial, long-term one between a mentor with significant experience and a mentee. Whether you are seeking to gain new skills from someone who has already travelled on the road of the journey you are just beginning, or if you are a seasoned professional seeking to give back by donating time, resources, advice and expertise to a younger version of yourself, Tutelage fosters an environment for professional life enrichment. </h2>
+
+                    <p></p>
+
+                    <h2 className="missionParagraphs">We enjoy when people enjoy the instruction,teaching and training they receive in conjunction with personalized coaching, guidance, nurturance, and encouragement from those they receive it from.</h2>
+                    
+                    <p></p>
+
+                    <h2 className="missionParagraphs">We specialize in creating "win-win" scenarios for both sides. The mentee recieves the crucial assistance needed to "level-up" their skills to improve their income, gain promotions that better their career trajectory, and helps them to navigate around the "potholes" of mistakes that others have encountered. The mentor, on the other hand, much more than intrinsic value, not only experiences the joys of altruism by selflessly giving to others in need, but also reinforces their own learning by way of teaching others, and may also even find themselves grooming the dream candidate that they were seeking to hire at their own business. </h2>
+
+                    <p></p>
+
+                    <h2 className="missionParagraphs">A Mentor is more than just a tutor. A mentor is a conduit of life transformation. The countless number of lives that have been enriched as a result of the meeting of the minds through our site and their impact on various industries is immeasurable. </h2>
+
+                    <p></p>
+
+                    <h2 className="missionParagraphs">Reach Out To Your Full Potential.</h2>
+                </div>
+
                 <div id="mainDiv">
-                    <div class="teamMember">
-                        <div class="teamMemberTitle">
+                    <div className="teamMember">
+                        <div className="teamMemberTitle">
                             <h2>Technical Lead</h2>
                         </div>
-                        <div class="teamMemberPhoto">
+                        <div className="teamMemberPhoto">
                             <img src="./images/GersonCropped.jpg" alt="Tech Lead" />
                         </div>
-                        <div class="teamMemberCaption">
+                        <div className="teamMemberCaption">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</p>
                         </div>
                     </div>
 
 
-                    <div class="teamMember">
-                        <div class="teamMemberTitle">
+                    <div className="teamMember">
+                        <div className="teamMemberTitle">
                             <h2>Product Manager</h2>
                         </div>
-                        <div class="teamMemberPhoto">
+                        <div className="teamMemberPhoto">
                             <img  src="./images/gregcropped2.jpg" alt="Product Manager" />
                         </div>
-                        <div class="teamMemberCaption">
+                        <div className="teamMemberCaption">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</p>
                         </div>
                     </div>
 
 
-                    <div class="teamMember">
-                        <div class="teamMemberTitle">
+                    <div className="teamMember">
+                        <div className="teamMemberTitle">
                             <h2>Design Lead</h2>
                         </div>
-                        <div class="teamMemberPhoto">
+                        <div className="teamMemberPhoto">
                             <img src="./images/CarolinaPic2.jpeg" alt="Design Lead" />
                         </div>
-                        <div class="teamMemberCaption">
+                        <div className="teamMemberCaption">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</p>
                         </div>
                     </div>
 
 
-                    <div class="teamMember">
-                        <div class="teamMemberTitle">
+                    <div className="teamMember">
+                        <div className="teamMemberTitle">
                             <h2>Demo Lead</h2>
                         </div>
-                        <div class="teamMemberPhoto">
+                        <div className="teamMemberPhoto">
                             <img  src="./images/EddiePic2.jpg" alt="Demo Lead" />
                         </div>
-                        <div class="teamMemberCaption">
+                        <div className="teamMemberCaption">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</p>
                         </div>
                     </div>
 
 
-                    <div class="teamMember">
-                        <div class="teamMemberTitle">
+                    <div className="teamMember">
+                        <div className="teamMemberTitle">
                             <h2>Scrum Master</h2>
                         </div>
-                        <div class="teamMemberPhoto">
+                        <div className="teamMemberPhoto">
                             <img src="./images/NickCropped.jpg" alt="Scrum Master" />
                         </div>
-                        <div class="teamMemberCaption">
+                        <div className="teamMemberCaption">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis ac sapien vitae interdum. Fusce mattis metus at vehicula volutpat. Nulla porttitor sed ipsum vel scelerisque. Phasellus elementum quam in dapibus feugiat. Nulla sed dolor non lectus sagittis varius. Praesent tincidunt, nisi id feugiat porta, risus purus luctus velit, quis tincidunt nunc dui sit amet metus. Donec efficitur interdum est, elementum sollicitudin odio tristique eget. Nunc consequat congue blandit. In hac habitasse platea dictumst. Suspendisse euismod ipsum tellus, id tristique eros tristique eget. Donec vel fermentum risus. Aliquam commodo mi et mauris pretium consectetur nec a purus. Quisque efficitur massa massa, nec imperdiet nulla pellentesque sit amet. Phasellus blandit purus et justo posuere, tincidunt egestas odio sollicitudin. Etiam non suscipit metus.</p>
                         </div>
                     </div>
@@ -101,26 +123,7 @@ class AboutUs extends Component {
 
                 </div>
 
-                <div id="missionStatementSection">
-                    <h1 id="missionStatement">Mission Statement</h1>
-                    <h2 className="missionParagraphs">Our mission at Tutelage is to serve as a tool to spark a connection, preferably a meaningful, mutually beneficial, long-term one between a mentor with significant experience and a mentee. Whether you are seeking to gain new skills from someone who has already travelled on the road of the journey you are just beginning, or if you are a seasoned professional seeking to give back by donating time, resources, advice and expertise to a younger version of yourself, Tutelage fosters an environment for professional life enrichment. </h2>
 
-                    <p></p>
-
-                    <h2 className="missionParagraphs">We enjoy when people enjoy the instruction,teaching and training they receive in conjunction with personalized coaching, guidance, nurturance, and encouragement from those they receive it from.</h2>
-                    
-                    <p></p>
-
-                    <h2 className="missionParagraphs">We specialize in creating "win-win" scenarios for both sides. The mentee recieves the crucial assistance needed to "level-up" their skills to improve their income, gain promotions that better their career trajectory, and helps them to navigate around the "potholes" of mistakes that others have encountered. The mentor, on the other hand, much more than intrinsic value, not only experiences the joys of altruism by selflessly giving to others in need, but also reinforces their own learning by way of teaching others, and may also even find themselves grooming the dream candidate that they were seeking to hire at their own business. </h2>
-
-                    <p></p>
-
-                    <h2 className="missionParagraphs">A Mentor is more than just a tutor. A mentor is a conduit of life transformation. The countless number of lives that have been enriched as a result of the meeting of the minds through our site and their impact on various industries is immeasurable. </h2>
-
-                    <p></p>
-
-                    <h2 className="missionParagraphs">Reach Out To Your Full Potential.</h2>
-                </div>
 
             </div>
 
