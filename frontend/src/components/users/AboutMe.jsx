60cebd844759321d6ef0daad6ed3ed0f3c6a60e8
eddieharmonjr <eddieharmon@ac.c4q.nyc>
2018-03-30 17:58:46 -0400

    update textarea field names to match their id for backend syncing.

--- frontend/src/components/users/AboutMe.jsx
+++ frontend/src/components/users/AboutMe.jsx
@@ -17,26 +17,26 @@ class AboutMe extends Component {
 
                 <div className="aboutMeQuestionAndAnswerSection">
                     <h2>Tell Us More About Yourself.</h2>
-                    <textarea name="" id="bio" cols="50" rows="10"></textarea><br/>
+                    <textarea name="bio" id="bio" cols="50" rows="10"></textarea><br/>
                     <button className="saveButtons" type="button">Save</button>
 
                     <h2>What Is Your Occupation?</h2>
-                    <textarea name="" id="occupation" cols="50" rows="10"></textarea><br/>
+                    <textarea name="occupation" id="occupation" cols="50" rows="10"></textarea><br/>
                     <button className="saveButtons" type="button">Save</button>
 
 
                     <h2>Do You Have Any Credentials?</h2>
-                    <textarea name="" id="credentials" cols="50" rows="10"></textarea><br/>
+                    <textarea name="credentials" id="credentials" cols="50" rows="10"></textarea><br/>
                     <button className="saveButtons" type="button">Save</button>
 
 
                     <h2>What Is Your Favorite Hobby?</h2>
-                    <textarea name="" id="hobbies" cols="50" rows="10"></textarea><br/>
+                    <textarea name="hobbies" id="hobbies" cols="50" rows="10"></textarea><br/>
                     <button className="saveButtons" type="button">Save</button>
 
 
                     <h2>Common Interests?</h2>
-                    <textarea name="" id="interests" cols="50" rows="10"></textarea><br/>
+                    <textarea name="interests" id="interests" cols="50" rows="10"></textarea><br/>
                     <button className="saveButtons" type="button">Save</button>
                 </div>
 
