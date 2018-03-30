305322a72a5364fe2e5e0e4c90cb0841a8e4e5d9
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-03-29 21:24:17 -0400

    added missing code that had been erased

--- frontend/src/components/login/RegisterUser.jsx
+++ frontend/src/components/login/RegisterUser.jsx
@@ -217,10 +217,9 @@ class RegisterUser extends Component {
                 onChange={handleInputChange}
                 required
               />
-              {message}
+              <div className="register-message">{message}</div>
               <input
-                className="input-box"
-                id="createAccountButton"
+                className="input-box createAccountButton"
                 type="submit"
                 value="Create Account"
               />
