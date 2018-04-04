3464acdddfdc92d1760472caf217b41ab43ab5f8
crestrepo12 <CarolinaRestrepo@ac.c4q.nyc>
2018-04-04 10:42:35 -0400

    added margin for chatbox in Profile

--- frontend/src/components/users/Profile.jsx
+++ frontend/src/components/users/Profile.jsx
@@ -83,99 +83,116 @@ class Profile extends Component {
   };
 
   showChatBoxHandle = () => {
-    const {showChatBox} = this.state;
+    const { showChatBox } = this.state;
     this.setState({
       showChatBox: !showChatBox
     });
-  }
+  };
 
   render() {
-    const { clearMessage, handleTextarea, checkReload, showChatBoxHandle } = this;
+    const {
+      clearMessage,
+      handleTextarea,
+      checkReload,
+      showChatBoxHandle
+    } = this;
     const { profileUser, userMessage, showChatBox } = this.state;
     const { user } = this.props;
-    let currentURL = this.props.match.url
-    console.log("THIS.PROPS...SMH", this.props)
-    console.log("profiel USER:::", profileUser)
-    console.log("profiel USER occupaion:::", profileUser.occupation)
+    let currentURL = this.props.match.url;
+    console.log("THIS.PROPS...SMH", this.props);
+    console.log("profiel USER:::", profileUser);
+    console.log("profiel USER occupaion:::", profileUser.occupation);
     let commonInterests = "";
 
     checkReload();
     return (
       <div id="user-profile" className="margin">
         <div className="background-banner">
-          <div className="sq2" />
+          <div className="color-sq2" />
           <div id="user-banner">
-            { 
-            <div className="image-crop margin">
-              <Link to = {`/users/${profileUser.username}/edit`} refresh = "true"> 
-                <img
-                  src={profileUser.imgurl}
-                  alt="profile picture"
-                  className="img-profile"
-                /> 
-              </Link>
+            <div className="user-pic-info">
+              {
+                <div className="image-crop margin">
+                  <Link
+                    to={`/users/${profileUser.username}/edit`}
+                    refresh="true"
+                  >
+                    <img
+                      src={profileUser.imgurl}
+                      alt="profile picture"
+                      className="img-profile"
+                    />
+                  </Link>
+                </div>
+              }
+              <div id="user-basic-info">
+                <div>
+                  <h1 className="user-header-name" ><strong>
+                    {`${profileUser.firstname} ${profileUser.lastname}`}
+                  </strong></h1>
+                
+                <h3> {profileUser.gender} </h3>
+                <h3> gender: {profileUser.gender} </h3>
+                <h3> zipcode: {profileUser.zipcode} </h3>
+                <h3> Occupation: {profileUser.occupation} </h3>
+                {/* <Link to='/survey'>Click here to edit your survey questions</Link> */}
+              </div>
+              </div>
             </div>
-            }
-            <div id="user-basic-info">
-              <h1 className="user-header">
-                <strong>{`${profileUser.firstname} ${
-                  profileUser.lastname
-                }`}
-                </strong>
-              </h1>
-              <h3> {profileUser.gender} </h3>
-              <h3> gender: {profileUser.gender} </h3>
-              <h3> zipcode: {profileUser.zipcode} </h3>
-              <h3> Occupation: {profileUser.occupation} </h3>
-              {/* <Link to='/survey'>Click here to edit your survey questions</Link> */}
+            <div className="box-two">
+              <div className="user-info-content">
+                <div id="quick-user-info">
+                  <div className="margin-top">
+                    Interests: {profileUser.commonInterests}
+                  </div>
+                  <div className="margin-top">
+                    {" "}
+                    Hobbies: {profileUser.hobbies}{" "}
+                  </div>
+                  <div className="margin-top"> Bio: {profileUser.bio}</div>
+                </div>
+                <div className="margin-top">
+                  Credentials: {profileUser.credentials}{" "}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="user-info-content">
-          <div id="quick-user-info" >
-          <div className="margin-top">Interests: {profileUser.commonInterests}</div>
-          <div className="margin-top"> Hobbies: {profileUser.hobbies} </div>
-          <div className="margin-top"> Bio: {profileUser.bio}</div>
-          </div>
-          <div className="margin-top">
-            Credentials: {profileUser.credentials}{" "}
-          </div>
         </div>
         {/* Chat box will display once they click Let's talk */}
-        { showChatBox ? 
-        (<div className="center">
-          <div id="chat-box" className="margin-top">
-            <label>
-              <h2> Let's connect: </h2>
-            </label>
-            <textarea
-              name="message"
-              id="message-box"
-              cols="30"
-              rows="5"
-              placeholder="Write your message here ..."
-              value={userMessage}
-              onChange={handleTextarea}
-            />
-            <div className="chat-buttons">
-              <input
-                type="submit"
-                value="Submit Message"
-                className="submit button-size"
+        {showChatBox ? (
+          <div className="center margin">
+            <div id="chat-box" className="margin-top">
+              <label>
+                <h2> Let's connect: </h2>
+              </label>
+              <textarea
+                name="message"
+                id="message-box"
+                cols="30"
+                rows="5"
+                placeholder="Write your message here ..."
+                value={userMessage}
+                onChange={handleTextarea}
               />
-              <button className="clear button-size" onClick={clearMessage}>
-                {" "}
-                Clear{" "}
-              </button>
+              <div className="chat-buttons">
+                <input
+                  type="submit"
+                  value="Submit Message"
+                  className="submit button-size"
+                />
+                <button className="clear button-size" onClick={clearMessage}>
+                  {" "}
+                  Clear{" "}
+                </button>
+              </div>
             </div>
           </div>
-        </div>) : (
+        ) : (
           <div className="center-chatbox">
-            <button 
-            className="submit button-size"
-            onClick={showChatBoxHandle}
-            > Let's Talk!
+            <button className="submit button-size" onClick={showChatBoxHandle}>
+              {" "}
+              Let's Talk!
             </button>
           </div>
         )}
